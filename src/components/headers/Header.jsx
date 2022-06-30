c15fa5d8e6069ea7307bbd882d06e35ad978847d
Rakesh <rakesh.bca49@gmail.com>
2022-06-30 17:52:41 +0530

    first changes

--- src/components/headers/Header.jsx
+++ src/components/headers/Header.jsx
@@ -1,30 +1,8 @@
 import { AppBar, Toolbar, Box, styled } from "@mui/material";
 import logo from "../../common/images/logo.jpeg";
 
-const StyledHeader = styled(AppBar)`
-  background: #008000;
-  height: 55px;
-`;
-const Component = styled(Box)`
-margin-left: 10%
-`
-
-
-
-
 const Header = () => {
-
-    // const logoURL= logo
-
-  return (
-    <StyledHeader>
-      <Toolbar>
-        <Component>
-          <img src={logo} alt="logo" style={{ width: 110 }}/>
-        </Component>
-      </Toolbar>
-    </StyledHeader>
-  );
+  return <div>hello header</div>;
 };
 
 export default Header;
